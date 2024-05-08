2ce509beacb68c06a8db4d4c698c04466d1163d4
vs2k5 <63846930+Smei2005@users.noreply.github.com>
2024-05-08 17:36:01 +0000

    fix main.py

--- main.py
+++ main.py
@@ -6,33 +6,37 @@ from WiFiQRCodeGenerator import WiFi
 from LocationQRCodeGenerator import Locate
 from ContactsQRCodeGenerator import Contacts
 
-selected = option_menu(
-    menu_title="Main",
-    options=["Text Data", "WiFi", "Location", "Contacts"],
-    icons=None,
-    menu_icon=None,
-    default_index=0,
-    orientation="horizontal"
-)
+def main():
+    selected = option_menu(
+        menu_title="Main",
+        options=["Text Data", "WiFi", "Location", "Contacts"],
+        icons=None,
+        menu_icon=None,
+        default_index=0,
+        orientation="horizontal"
+    )
 
 
-if selected == "Text Data":
-    with st.container(height=1000):
-        file_name = Text.select(selected)
-        qr_reader.image(file_name)
+    if selected == "Text Data":
+        with st.container(height=1000):
+            file_name = Text.select(selected)
+            qr_reader.image(file_name)
 
 
-if selected == "WiFi":
-    with st.container(height=750):
-        file_name = WiFi.select(selected)
-        qr_reader.image(file_name)
+    if selected == "WiFi":
+        with st.container(height=750):
+            file_name = WiFi.select(selected)
+            qr_reader.image(file_name)
 
-if selected == "Location":
-    with st.container(height=750):
-        file_name = Locate.select(selected)
-        qr_reader.image(file_name)
+    if selected == "Location":
+        with st.container(height=750):
+            file_name = Locate.select(selected)
+            qr_reader.image(file_name)
 
-if selected == "Contacts":
-    with st.container(height=820):
-        file_name = Contacts.select(selected)
-        qr_reader.image(file_name)
+    if selected == "Contacts":
+        with st.container(height=820):
+            file_name = Contacts.select(selected)
+            qr_reader.image(file_name)
+
+if __name__ == "__main__":
+    main()
